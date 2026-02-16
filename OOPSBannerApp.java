292509505bb0ce6030071f768a4fcab7c5ae8eb0
dd0417-sys <dd0417@srmist.edu.in>
2026-02-16 22:07:53 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,6 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
-        System.out.println("  *****   " + "  *****   " + " ******** " + " ******** ");
-        System.out.println(" *     *  " + " *     *  " + " *       *" + " *        ");
-        System.out.println("*       * " + "*       * " + " *       *" + " *        ");
-        System.out.println("*       * " + "*       * " + " ******** " + " ******** ");
-        System.out.println("*       * " + "*       * " + " *        " + "        * ");
-        System.out.println(" *     *  " + " *     *  " + " *        " + "        * ");
-        System.out.println("  *****   " + "  *****   " + " *        " + " ******** ");
+        System.out.println("OOPS");
     }
 }
