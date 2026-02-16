a9c173a8eaa248f6f936ec3233b09361fd8dba3d
dd0417-sys <dd0417@srmist.edu.in>
2026-02-16 21:46:30 +0530

    Create OOPSBannerApp.java

---
+++ OOPSBannerApp.java
@@ -0,0 +1,8 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
+
+UC1: Implemented OOPSBannerApp to print OOPS to console
